69c4c75538a2a9bad9d9aed092de5d59056890c4
54-xenon <congas.rabbi_0b@icloud.com>
2025-11-24 15:24:02 +0900

    DBを変更

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:ankicards/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 
-void main(){
-  runApp(MyApp());
+
+// Hive(DBの初期化)
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
